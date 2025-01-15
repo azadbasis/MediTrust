668594a13b25b80473f3792b125ca4491191fc8c
azadbasis <azadbasis@gmail.com>
2025-01-16 00:38:34 +0600

    Improved modularity and maintainability of layout configuration.

--- app/src/main/java/com/meditrust/findadoctor/core/data/source/remote/ChatGPTApi.kt
+++ app/src/main/java/com/meditrust/findadoctor/core/data/source/remote/ChatGPTApi.kt
@@ -1,13 +1,6 @@
 package com.meditrust.findadoctor.core.data.source.remote
 
 
-import com.meditrust.findadoctor.core.data.model.ChatGPTRequest
-import com.meditrust.findadoctor.core.data.model.ChatGPTResponse
-import retrofit2.Call
-
-import retrofit2.http.Body
-import retrofit2.http.Headers
-import retrofit2.http.POST
 
 interface ChatGPTApi {
 
