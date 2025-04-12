564263e4b83ec2c1806c18b6c7844ecc126a2a22
azadbasis <azadbasis@gmail.com>
2025-04-12 13:39:35 +0600

    View profile container add with all user type

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,12 +66,14 @@ dependencies {
     val nav_version = "2.8.0"
     implementation("androidx.navigation:navigation-fragment:$nav_version")
     implementation("androidx.navigation:navigation-ui:$nav_version")
+    
     // Import the BoM for the Firebase platform
     implementation(platform("com.google.firebase:firebase-bom:33.2.0"))
     // When using the BoM, you don't specify versions in Firebase library dependencies
     implementation("com.google.firebase:firebase-auth")
     implementation("androidx.datastore:datastore-preferences:1.1.1")
     implementation("com.google.firebase:firebase-storage")
+    
     // ViewModel +  LiveData+ Lifecycles only (without ViewModel or LiveData)+ Saved state module for ViewModel
     val lifecycle_version = "2.6.1"
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
@@ -79,7 +81,10 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycle_version")
     implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:$lifecycle_version")
 
+    // Splash Screen
     implementation("androidx.core:core-splashscreen:1.0.0")
+
+    // Intuit SDP and SSP
     implementation("com.intuit.sdp:sdp-android:1.1.1")
     implementation("com.intuit.ssp:ssp-android:1.1.1")
 
@@ -95,19 +100,20 @@ dependencies {
     //Timber
     implementation("com.jakewharton.timber:timber:5.0.1")
 
-    // CameraX core library using the camera2 implementation
+    // CameraX core library ,CameraX Lifecycle library,use the CameraX View class
     val camerax_version = "1.5.0-alpha02"
     implementation("androidx.camera:camera-camera2:${camerax_version}")
-    // If you want to additionally use the CameraX Lifecycle library
     implementation("androidx.camera:camera-lifecycle:${camerax_version}")
-    // If you want to additionally use the CameraX View class
     implementation("androidx.camera:camera-view:${camerax_version}")
 
     //Glide
     implementation ("com.github.bumptech.glide:glide:4.16.0")
     implementation ("androidx.recyclerview:recyclerview:1.3.2")
+
+    //circle image view
     implementation ("de.hdodenhof:circleimageview:3.1.0")
 
+    // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
